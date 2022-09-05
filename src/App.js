3dbbd369c8db818eb0ad03ef72d7ca4da950b767
Aleksa <obradovic.aleksa@gmail.com>
2022-09-05 21:46:01 +0200

    definicije

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+/*
+  useState hook je posebna funkcija koja omogućava da se zakacimo na React funkcije. 
+  useState je hook koja vam omogućava da dodamo React stanje komponentama funkcije
+
+  koristi se kada:
+  na primer: napišemo komponentu funkcije i shvatimo da moramo da joj dodamo neko stanje, 
+  prethodno smo morali da je konvertujemo u klasu. 
+  Sada možemo koristiti Hook unutar postojeće komponente funkcije
+
+*/
+
+
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
